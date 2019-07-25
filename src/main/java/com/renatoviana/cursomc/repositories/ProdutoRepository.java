01d9b8cf6aac0fbed28df4b4a56e58fc7f014fc6
Renato Viana <renatoviana30@gmail.com>
2019-07-25 09:42:59 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/renatoviana/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/renatoviana/cursomc/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.renatoviana.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.renatoviana.cursomc.domain.Categoria;
+import com.renatoviana.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	
 }
